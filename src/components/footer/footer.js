4a9e5a506cabacd2ff39bfaf3ba95537ecc3254c
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-03-11 03:40:31 +0530

    UI-init

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,9 @@
 export const Footer = () => (
     <footer>
-        <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Powered by{' '}
-            <span>
-                <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
+        © 2023 Created by:
+        <a href="https://github.com/Ritwikrajsingh" target='_blank'>
+            {' Ritwik Raj Singh, '}
         </a>
+        built with <a href="https://nextjs.org" target='_blank'>NEXT.js</a>
     </footer>
 )
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,17 +5,18 @@ import Link from 'next/link'
 export const Header = () => {
     return (
         <header>
-            <div className='nav-bar'>
-                <Image alt='logo' src='/next.svg' width={80} height={30} />
-                <nav>
-                    <ul>
-                        <li><Link href='/'>Home</Link></li>
-                        <li><Link href='/events'>Events</Link></li>
-                        <li><Link href='/about'>About</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            <h1>de Finibus Bonorum et Malorum</h1>
+            <div>
+                <div className='nav-bar'>
+                    <Image alt='logo' src='/codemon.svg' width={150} height={30} />
+                    <nav>
+                        <ul>
+                            <li><Link href='/'>Home</Link></li>
+                            <li><Link href='/events'>Events</Link></li>
+                            <li><Link href='/about'>About</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1>de Finibus Bonorum et Malorum</h1></div>
         </header>
     )
 }
